a7721d8ced72aeeca40a4bf14f61b6353c608996
Shani <shani.babu@binplus.in>
2026-07-07 14:13:49 +0530

    some changes in index.js file

--- gateway/index.js
+++ gateway/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send(`Hello from ${process.env.SERVER} gateway server using CI/CD !`);
+    res.send(`Hello from ${process.env.SERVER} gateway server using CI/CD pipeline!`);
 });
 
 app.use("/product", proxy("http://product:3003"));
