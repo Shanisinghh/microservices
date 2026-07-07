59ac5a48a9a05eceffdd8df330bbbb77c300d691
Shani <shani.babu@binplus.in>
2026-07-07 15:10:58 +0530

    add some product

--- product/index.js
+++ product/index.js
@@ -29,6 +29,7 @@ app.get("/products", (req, res) => {
         { id: 9, name: "Product 9", price: 90 },
         { id: 10, name: "Product 10", price: 100 },
         { id: 11, name: "Product 11111", price: 110 },
+        { id: 12, name: "Product 12222", price: 120 },
     ];
     res.json(products);
 });
